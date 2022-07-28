c45a0f9140cbbced972ab9743d6cc66d9652dc78
Quynh <dntquynh@gmail.com>
2022-07-28 10:07:30 +1000

    Add formatting with string methods

---
+++ format_with_string_methods.rb
@@ -0,0 +1,9 @@
+first_name = "quynh"
+last_name = "do"
+city = "sydney"
+state = "nsw"
+first_name = first_name.capitalize!
+last_name.capitalize!
+city.capitalize!
+state.upcase!
+puts "Your name is #{first_name} #{last_name}. You are from #{city}, #{state}"
